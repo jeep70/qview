d966631eea5127aa3e359c78c4b506b162e03a36
Benjamin O <jeep70cp@gmail.com>
2021-05-22 11:03:23 -0400

    ampersand protocol?

--- ci/pwsh/build.ps1
+++ ci/pwsh/build.ps1
@@ -6,7 +6,7 @@ param
 )
 
 if ($IsWindows) {
-    pwsh -File $env:BUILD_REPOSITORY_LOCALPATH/ci/pwsh/vcvars.ps1
+    & "$env:BUILD_REPOSITORY_LOCALPATH/ci/pwsh/vcvars.ps1"
 }
 
 qmake $args[0] PREFIX=$Prefix
--- ci/pwsh/buildapng.ps1
+++ ci/pwsh/buildapng.ps1
@@ -11,7 +11,7 @@ git checkout $(git tag | select -last 1)
 
 # vcvars on windows
 if ($IsWindows) {
-    pwsh -File $env:BUILD_REPOSITORY_LOCALPATH/ci/pwsh/vcvars.ps1
+    & "$env:BUILD_REPOSITORY_LOCALPATH/ci/pwsh/vcvars.ps1"
 }
 
 qmake "CONFIG += libpng_static"
